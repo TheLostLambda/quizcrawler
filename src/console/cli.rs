c1ebc6d838d9242bb0f63130b7b77551e9fdf025
Brooks J Rady <b.j.rady@gmail.com>
2020-04-06 15:54:05 +0100

    It's a mess, but it's a start

--- src/console/cli.rs
+++ src/console/cli.rs
@@ -1,17 +1,16 @@
-// use crate::core::data::QuestionVariant;
-use crate::console::util::*;
 use crate::core::games::Game;
 use crate::core::games::*;
 use crate::crawler::data::Crawler;
-use std::char;
+use crossterm::{terminal, ExecutableCommand};
 use std::collections::HashMap;
 use std::error::Error;
-use std::fmt::Write;
 use std::fs;
-use std::io;
+use std::io::{self, Write};
 use structopt::StructOpt;
-use termion::event::Key; // Do I really want this here?
-use termion::*; // PURGE ME
+use tui::backend::CrosstermBackend;
+use tui::widgets::{Block, Borders, Widget};
+use tui::Terminal;
+// Tend to these imports! ^
 
 #[derive(StructOpt)]
 #[structopt(
@@ -30,130 +29,169 @@ struct QC {
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let args = QC::from_args();
-    let parse_config = fs::read_to_string(&args.recipe)?;
-    let crawler = Crawler::new(&parse_config)?;
-    let tree = crawler.parse_file(&args.notes);
-    // Press i or ? for more info about the selected item?
-    let mut path = Vec::new();
-    let mut sel_hist = HashMap::new();
-    loop {
-        let node = tree.child_at_path(&path).unwrap();
-        if !node.is_parent() {
-            play_game(MultipleChoice::new(
-                MCConfig { flipped: args.flipped },
-                &node.questions,
-            ));
-            path.pop();
-            continue;
-        }
-        let children: Vec<_> = node.children.iter().map(|x| &x.name).collect();
-        new_screen();
+    let crawler_recipe = fs::read_to_string(&args.recipe)?;
+    let crawler = Crawler::new(&crawler_recipe)?;
+    let notes = crawler.parse_file(&args.notes);
 
-        print!("{} > ", tree.name);
-        for node in &path {
-            print!("{} > ", node);
-        }
+    terminal::enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    stdout.execute(terminal::EnterAlternateScreen)?;
 
-        let selected = sel_hist.entry(path.clone()).or_insert(0);
-        // ^ I think that the clone here is okay
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-        // Show more and make this nicer
-        let selected_node = node.child_at_path(&[children[*selected]]).unwrap();
-        float_right(&format!(
-            "{} Children, {} Questions",
-            selected_node.children.len(),
-            selected_node.questions.len()
-        ));
+    terminal.draw(|mut f| {
+        let size = f.size();
+        Block::default()
+            .title("Block")
+            .borders(Borders::ALL)
+            .render(&mut f, size);
+    })?;
 
-        println!("\n");
+    std::thread::sleep_ms(2000);
 
-        for (id, child) in children.iter().enumerate() {
-            // Not a fan of the clone here...
-            if id == *selected {
-                print!(">")
-            } else {
-                print!(" ");
-            }
-            println!(" {}", child);
-        }
+    // SelectableList::default()
+    //     .block(Block::default().title("SelectableList").borders(Borders::ALL))
+    //     .items(&["Item 1", "Item 2", "Item 3"])
+    //     .select(Some(1))
+    //     .style(Style::default().fg(Color::White))
+    //     .highlight_style(Style::default().modifier(Modifier::ITALIC))
+    //     .highlight_symbol(">>");
 
-        match get_valid_key(&[Key::Up, Key::Down, Key::Right, Key::Left]) {
-            Key::Up if node.is_parent() && *selected > 0 => *selected -= 1,
-            Key::Down if node.is_parent() && *selected < children.len() - 1 => *selected += 1,
-            Key::Right if node.is_parent() => path.push(&children[*selected]),
-            Key::Left => { path.pop(); }, // This is an alternative to drop(path.pop())
-            Key::Null => graceful_death(),
-            _ => continue,
-        };
-    }
+    terminal
+        .backend_mut()
+        .execute(terminal::LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }
 
-fn play_game(mut game: impl Game) {
-    loop {
-        // Clear the screen before the next question
-        new_screen();
-        let (remaining, seen, score) = game.progress();
-        print!("{} flashcards to go!", remaining);
-        if seen > 0 {
-            float_right(&format!("Your score is {:.2}%", score));
-        }
+// pub fn run() -> Result<(), Box<dyn Error>> {
+//     let args = QC::from_args();
+//     let parse_config = fs::read_to_string(&args.recipe)?;
+//     let crawler = Crawler::new(&parse_config)?;
+//     let tree = crawler.parse_file(&args.notes);
+//     // Press i or ? for more info about the selected item?
+//     let mut path = Vec::new();
+//     let mut sel_hist = HashMap::new();
+//     loop {
+//         let node = tree.child_at_path(&path).unwrap();
+//         if !node.is_parent() {
+//             play_game(MultipleChoice::new(
+//                 MCConfig { flipped: args.flipped },
+//                 &node.questions,
+//             ));
+//             path.pop();
+//             continue;
+//         }
+//         let children: Vec<_> = node.children.iter().map(|x| &x.name).collect();
+//         new_screen();
 
-        let question = match game.next_question() {
-            Some(q) => q,
-            None => break,
-        };
+//         print!("{} > ", tree.name);
+//         for node in &path {
+//             print!("{} > ", node);
+//         }
 
-        println!("\n{}", question);
+//         let selected = sel_hist.entry(path.clone()).or_insert(0);
+//         // ^ I think that the clone here is okay
 
-        println!();
+//         // Show more and make this nicer
+//         let selected_node = node.child_at_path(&[children[*selected]]).unwrap();
+//         float_right(&format!(
+//             "{} Children, {} Questions",
+//             selected_node.children.len(),
+//             selected_node.questions.len()
+//         ));
 
-        let choices = game.get_choices();
-        let (result, correct_ans) = if !choices.is_empty() {
-            let id_shift: u8 = 49;
-            let mut screen = String::new(); // Eventually buffer the whole screen here
-            let mut valid = Vec::new();
-            for (i, opt) in choices.iter().enumerate() {
-                let id = char::from(i as u8 + id_shift);
-                writeln!(&mut screen, "{}) {} ", id, opt).unwrap(); // Handle errors?
-                valid.push(id);
-            }
+//         println!("\n");
 
-            println!("{}", screen); // Replace this with a scrolling wrapper
+//         for (id, child) in children.iter().enumerate() {
+//             // Not a fan of the clone here...
+//             if id == *selected {
+//                 print!(">")
+//             } else {
+//                 print!(" ");
+//             }
+//             println!(" {}", child);
+//         }
 
-            // The char to digit thing here is hacky
-            let choice_idx = if let Some(chr) = get_valid_char(&valid) {
-                chr as u8 - id_shift
-            } else {
-                return;
-            };
-            // char::from(u8)
-            backtrack(screen.lines().count() as u16 + 1);
-            game.answer(&choice_idx.to_string())
-        } else {
-            let mut ans = String::new();
-            io::stdin().read_line(&mut ans).unwrap();
-            game.answer(&ans)
-        };
+//         match get_valid_key(&[Key::Up, Key::Down, Key::Right, Key::Left]) {
+//             Key::Up if node.is_parent() && *selected > 0 => *selected -= 1,
+//             Key::Down if node.is_parent() && *selected < children.len() - 1 => *selected += 1,
+//             Key::Right if node.is_parent() => path.push(&children[*selected]),
+//             Key::Left => { path.pop(); }, // This is an alternative to drop(path.pop())
+//             Key::Null => graceful_death(),
+//             _ => continue,
+//         };
+//     }
+// }
 
-        let ans_string = format!(", the answer is: {}", correct_ans);
-        // Clear the choices before showing the answer. Keep the definition
+// fn play_game(mut game: impl Game) {
+//     loop {
+//         // Clear the screen before the next question
+//         new_screen();
+//         let (remaining, seen, score) = game.progress();
+//         print!("{} flashcards to go!", remaining);
+//         if seen > 0 {
+//             float_right(&format!("Your score is {:.2}%", score));
+//         }
 
-        if result {
-            // Find a cleaner way to do this, maybe with a macro with this all
-            // predefined (all the {em} bits).
-            print!(
-                "{em}{g}Well done",
-                em = style::Bold,
-                g = color::Fg(color::Green),
-            );
-        } else {
-            print!("{em}{r}Sorry", em = style::Bold, r = color::Fg(color::Red),);
-        }
+//         let question = match game.next_question() {
+//             Some(q) => q,
+//             None => break,
+//         };
 
-        println!("{}{re}", ans_string, re = style::Reset);
+//         println!("\n{}", question);
 
-        if override_prompt(!result) {
-            game.i_was_right();
-        }
-    }
-}
+//         println!();
+
+//         let choices = game.get_choices();
+//         let (result, correct_ans) = if !choices.is_empty() {
+//             let id_shift: u8 = 49;
+//             let mut screen = String::new(); // Eventually buffer the whole screen here
+//             let mut valid = Vec::new();
+//             for (i, opt) in choices.iter().enumerate() {
+//                 let id = char::from(i as u8 + id_shift);
+//                 writeln!(&mut screen, "{}) {} ", id, opt).unwrap(); // Handle errors?
+//                 valid.push(id);
+//             }
+
+//             println!("{}", screen); // Replace this with a scrolling wrapper
+
+//             // The char to digit thing here is hacky
+//             let choice_idx = if let Some(chr) = get_valid_char(&valid) {
+//                 chr as u8 - id_shift
+//             } else {
+//                 return;
+//             };
+//             // char::from(u8)
+//             backtrack(screen.lines().count() as u16 + 1);
+//             game.answer(&choice_idx.to_string())
+//         } else {
+//             let mut ans = String::new();
+//             io::stdin().read_line(&mut ans).unwrap();
+//             game.answer(&ans)
+//         };
+
+//         let ans_string = format!(", the answer is: {}", correct_ans);
+//         // Clear the choices before showing the answer. Keep the definition
+
+//         if result {
+//             // Find a cleaner way to do this, maybe with a macro with this all
+//             // predefined (all the {em} bits).
+//             print!(
+//                 "{em}{g}Well done",
+//                 em = style::Bold,
+//                 g = color::Fg(color::Green),
+//             );
+//         } else {
+//             print!("{em}{r}Sorry", em = style::Bold, r = color::Fg(color::Red),);
+//         }
+
+//         println!("{}{re}", ans_string, re = style::Reset);
+
+//         if override_prompt(!result) {
+//             game.i_was_right();
+//         }
+//     }
+// }
--- src/console/util.rs
+++ src/console/util.rs
@@ -1,81 +1,81 @@
-use std::io;
-use std::io::Write;
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use termion::*;
+// use std::io;
+// use std::io::Write;
+// use termion::event::Key;
+// use termion::input::TermRead;
+// use termion::raw::IntoRawMode;
+// use termion::*;
 
-/// This clears the screen and moves the cursor to (1,1)
-pub fn new_screen() {
-    print!("{}{}", clear::All, cursor::Goto(1, 1));
-}
+// /// This clears the screen and moves the cursor to (1,1)
+// pub fn new_screen() {
+//     print!("{}{}", clear::All, cursor::Goto(1, 1));
+// }
 
-/// Deletes N lines from above the current position
-pub fn backtrack(n: u16) {
-    print!("{}\r{}", cursor::Up(n), clear::AfterCursor);
-}
+// /// Deletes N lines from above the current position
+// pub fn backtrack(n: u16) {
+//     print!("{}\r{}", cursor::Up(n), clear::AfterCursor);
+// }
 
-pub fn get_valid_char(valid: &[char]) -> Option<char> {
-    let keys: Vec<_> = valid.iter().map(|&c| Key::Char(c)).collect();
-    // This feels a bit verbose
-    if let Key::Char(chr) = get_valid_key(&keys[..]) {
-        Some(chr)
-    } else {
-        None
-    }
-}
+// pub fn get_valid_char(valid: &[char]) -> Option<char> {
+//     let keys: Vec<_> = valid.iter().map(|&c| Key::Char(c)).collect();
+//     // This feels a bit verbose
+//     if let Key::Char(chr) = get_valid_key(&keys[..]) {
+//         Some(chr)
+//     } else {
+//         None
+//     }
+// }
 
-// Use option instead of Key::Null?
-pub fn get_valid_key(valid: &[Key]) -> Key {
-    let mut stdout = io::stdout().into_raw_mode().unwrap();
-    write!(stdout, "{}", cursor::Hide).unwrap();
-    stdout.flush().unwrap();
-    for k in io::stdin().keys() {
-        match k.unwrap() {
-            Key::Char('q') => return Key::Null,
-            k if valid.contains(&k) => return k,
-            _ => continue,
-        }
-    }
-    Key::Null
-}
+// // Use option instead of Key::Null?
+// pub fn get_valid_key(valid: &[Key]) -> Key {
+//     let mut stdout = io::stdout().into_raw_mode().unwrap();
+//     write!(stdout, "{}", cursor::Hide).unwrap();
+//     stdout.flush().unwrap();
+//     for k in io::stdin().keys() {
+//         match k.unwrap() {
+//             Key::Char('q') => return Key::Null,
+//             k if valid.contains(&k) => return k,
+//             _ => continue,
+//         }
+//     }
+//     Key::Null
+// }
 
-/// Floats text to be right-aligned
-// Make this less brittle
-pub fn float_right(text: &str) {
-    let (c, _) = terminal_size().unwrap();
-    print!("\r{}{}", cursor::Right(c - text.len() as u16), text);
-}
+// /// Floats text to be right-aligned
+// // Make this less brittle
+// pub fn float_right(text: &str) {
+//     let (c, _) = terminal_size().unwrap();
+//     print!("\r{}{}", cursor::Right(c - text.len() as u16), text);
+// }
 
-// Make this return an Option. When quitting, return None so that the parent
-// function can handle that fallout. As it stands now, this just nukes the whole
-// program, so there is no "back" button
-pub fn override_prompt(wrong: bool) -> bool {
-    print!("ENTER or SPACE to continue");
-    if wrong {
-        println!(", 'o' for manual override...");
-    } else {
-        println!("...")
-    }
-    let mut stdout = io::stdout().into_raw_mode().unwrap();
-    write!(stdout, "{}", cursor::Hide).unwrap();
-    stdout.flush().unwrap();
-    // Should this be using get_valid_key()?
-    for k in io::stdin().keys() {
-        match k.unwrap() {
-            Key::Char('q') => graceful_death(),
-            Key::Char('\n') | Key::Char(' ') => return false,
-            Key::Char('o') if wrong => return true,
-            _ => continue,
-        }
-    }
-    false // Shouldn't be here... Use break and loop?
-}
+// // Make this return an Option. When quitting, return None so that the parent
+// // function can handle that fallout. As it stands now, this just nukes the whole
+// // program, so there is no "back" button
+// pub fn override_prompt(wrong: bool) -> bool {
+//     print!("ENTER or SPACE to continue");
+//     if wrong {
+//         println!(", 'o' for manual override...");
+//     } else {
+//         println!("...")
+//     }
+//     let mut stdout = io::stdout().into_raw_mode().unwrap();
+//     write!(stdout, "{}", cursor::Hide).unwrap();
+//     stdout.flush().unwrap();
+//     // Should this be using get_valid_key()?
+//     for k in io::stdin().keys() {
+//         match k.unwrap() {
+//             Key::Char('q') => graceful_death(),
+//             Key::Char('\n') | Key::Char(' ') => return false,
+//             Key::Char('o') if wrong => return true,
+//             _ => continue,
+//         }
+//     }
+//     false // Shouldn't be here... Use break and loop?
+// }
 
-pub fn graceful_death() {
-    let mut term = io::stdout().into_raw_mode().unwrap();
-    write!(term, "{}", cursor::Show).unwrap();
-    term.flush().unwrap();
-    term.suspend_raw_mode().unwrap();
-    std::process::exit(0);
-}
+// pub fn graceful_death() {
+//     let mut term = io::stdout().into_raw_mode().unwrap();
+//     write!(term, "{}", cursor::Show).unwrap();
+//     term.flush().unwrap();
+//     term.suspend_raw_mode().unwrap();
+//     std::process::exit(0);
+// }
